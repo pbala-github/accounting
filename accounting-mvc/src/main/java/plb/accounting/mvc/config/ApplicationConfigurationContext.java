dc6865d0025ff9d9ce6947845efd5a4d26dea443
pbala <bpanagiwtis@gmail.com>
2012-12-17 22:50:11 +0200

    configure spring web flow

--- accounting-mvc/src/main/java/plb/accounting/mvc/config/ApplicationConfigurationContext.java
+++ accounting-mvc/src/main/java/plb/accounting/mvc/config/ApplicationConfigurationContext.java
@@ -1,9 +1,15 @@
 package plb.accounting.mvc.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.faces.mvc.JsfView;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
+import org.springframework.webflow.executor.FlowExecutor;
+import org.springframework.webflow.mvc.servlet.FlowHandlerAdapter;
+import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
 
 /**
  * User: pbala
@@ -11,8 +17,36 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @Configuration
 @EnableWebMvc
+@ImportResource("classpath:/spring/webflow-config.xml")
 public class ApplicationConfigurationContext {
 
+    @Autowired
+    private FlowExecutor flowExecutor;
+    @Autowired
+    private FlowDefinitionRegistry flowDefinitionRegistry;
+
+    @Bean
+    public ViewResolver jsfViewResolver(){
+        UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+        viewResolver.setPrefix("WEB-INF/views/");
+        viewResolver.setSuffix(".xhtml");
+        viewResolver.setViewClass(JsfView.class);
+        return viewResolver;
+    }
+
+    @Bean
+    public FlowHandlerAdapter flowHandlerAdapter(){
+        FlowHandlerAdapter flowHandlerAdapter = new FlowHandlerAdapter();
+        flowHandlerAdapter.setFlowExecutor(flowExecutor);
+        return flowHandlerAdapter;
+    }
+
+    @Bean
+    public FlowHandlerMapping flowHandlerMapping(){
+        FlowHandlerMapping flowHandlerMapping = new FlowHandlerMapping();
+        flowHandlerMapping.setFlowRegistry(flowDefinitionRegistry);
+        return flowHandlerMapping;
+    }
 
     @Configuration
     @Profile(value = "production")
