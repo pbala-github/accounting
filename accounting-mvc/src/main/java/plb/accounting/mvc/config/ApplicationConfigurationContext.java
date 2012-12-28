813361e45c6e52a5e108b7e5b23451636e91a7f7
pbala <Panagiotis.BALA@intrasoft-intl.com>
2012-12-28 11:20:59 +0200

    add localization support, create standard template

--- accounting-mvc/src/main/java/plb/accounting/mvc/config/ApplicationConfigurationContext.java
+++ accounting-mvc/src/main/java/plb/accounting/mvc/config/ApplicationConfigurationContext.java
@@ -1,11 +1,19 @@
 package plb.accounting.mvc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.*;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.faces.mvc.JsfView;
 import org.springframework.faces.webflow.JsfFlowHandlerAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.executor.FlowExecutor;
@@ -20,7 +28,7 @@ import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
 @EnableWebMvc
 @ComponentScan(basePackages = {"plb.accounting.mvc.controller"})
 @ImportResource({"/WEB-INF/config/faces-context.xml","/WEB-INF/config/webflow-config.xml"})
-public class ApplicationConfigurationContext {
+public class ApplicationConfigurationContext extends WebMvcConfigurerAdapter{
 
     @Autowired
     private FlowExecutor flowExecutor;
@@ -39,6 +47,31 @@ public class ApplicationConfigurationContext {
         FlowHandlerMapping flowHandlerMapping = new FlowHandlerMapping();
         flowHandlerMapping.setFlowRegistry(flowDefinitionRegistry);
         return flowHandlerMapping;
+    }
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/resourceBundles/applMessages");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public HandlerInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
     @Configuration
