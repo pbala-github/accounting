22169358add9c8d3b92a5c7af51ee858eec3cfd4
pbala <Panagiotis.BALA@intrasoft-intl.com>
2013-03-14 17:16:33 +0200

    jeeunit config

--- accounting-integration-tests/src/test/java/plb/accounting/dao/test/JpaAccountDAOTest.java
+++ accounting-integration-tests/src/test/java/plb/accounting/dao/test/JpaAccountDAOTest.java
@@ -1,6 +1,8 @@
 package plb.accounting.dao.test;
 
+import com.googlecode.jeeunit.JeeunitRunner;
 import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
 import plb.accounting.dao.AccountDAO;
 import plb.accounting.dao.impl.jpa.JpaAccountDAO;
 
@@ -8,6 +10,7 @@ import plb.accounting.dao.impl.jpa.JpaAccountDAO;
  * User: pbala
  * Date: 3/11/13 4:48 PM
  */
+@RunWith(JeeunitRunner.class)
 public class JpaAccountDAOTest extends AbstractAccountDAOTest{
 
     private static AccountDAO dao;
